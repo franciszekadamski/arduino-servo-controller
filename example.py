3b25ae707f20e7af53f32fd28e915a2d8fae825f
Franciszek Adamski <adams.franciszek@wp.pl>
2025-01-24 18:13:33 +0100

    the same

--- example.py
+++ example.py
@@ -3,10 +3,9 @@
 from servo_board import ServoBoard
 
 def main():
-    board = ServoBoard()
+    board = ServoBoard(verbose=True)
     for pin in range(12, 14):
         for angle in [0, 45, 90, 135, 180, 90, 0]:
-            print(f"Moving servo {pin} to position {angle}")
             board.set_position(pin, angle)
 
 if __name__ == "__main__":
