ea9cb7113b38a12627cbb978e01a978ad44557e5
Franciszek Adamski <adams.franciszek@wp.pl>
2025-02-12 22:29:27 +0100

    add treshold function and timer for the controller

--- lib/servo_controller.c
+++ lib/servo_controller.c
@@ -22,6 +22,8 @@ int servo_pin;
 int servo_angle;
 
 // settings
+int mode = 1;
+
 int water_valve_angle_open = 90;
 int water_valve_angle_close = 45;
 
@@ -30,7 +32,10 @@ int light_switch_angle_close = 45;
 
 unsigned long int light_switch_open_time_ms = 1000 * 60 * 60 * 16; // 57600 000 ms
 unsigned long int light_switch_period_time_ms = 1000 * 60 * 60 * 24; // 86400 000 ms
-unsigned long int light_switch_close_time_ms = light_switch_period_time_ms - ligth_switch_open_time_ms;
+unsigned long int light_switch_close_time_ms = light_switch_period_time_ms - light_switch_open_time_ms;
+
+unsigned long int water_valve_open_time_ms = 1000 * 5;
+unsigned long int water_valve_closed_time_ms = 1000 * 60 * 10;
 
 int watering_state = 0; // 0: no watering; 1: watering;
 int lighting_state = 0; // 0: light off; 1: light on
@@ -38,8 +43,8 @@ int lighting_state = 0; // 0: light off; 1: light on
 int watering_state_changed = 0;
 int lighting_state_changed = 0;
 
-int air_humidity_treshold = 85;
-int soil_humidit_treshold = 50;
+double air_humidity_treshold = 120.0;
+double soil_humidit_treshold = 50.0;
 unsigned long int post_watering_wait_time_ms = 1000 * 60 * 10; // 600 000 ms
 unsigned long int watering_time_ms = 30 * 1000;
 
@@ -62,20 +67,20 @@ void setup() {
 }
 
 
-void loop() {
-  // read sensors
-  double air_humidity = analogRead(air_humidity_sensor_pin) * 1.9 / 3.3;
-  double soil_humidity = 104 - (analogRead(soil_humidity_sensor_pin) / 7);
-  int air_temperature = analogRead(air_temperature_sensor_pin);
-
-  // send data
+void sendSensorData(
+  double air_humidity,
+  double soil_humidity,
+  int air_temperature
+) {
   Serial.print(air_humidity);
   Serial.print('-');
   Serial.print(soil_humidity);
   Serial.print('-');
-  Serial.println(air_temperature);
+  Serial.println(air_temperature);  
+}
 
-  // receive control signals
+
+void receiveControlMessage() {
   while (Serial.available() > 0) {
     in_message = Serial.readStringUntil('\n');
     if(in_message.length() > 0) {
@@ -92,77 +97,81 @@ void loop() {
       }
     }
   }
-
-  // check conditions and modify state
-  lighting_state_changed = 0;
-  if (lighting_state == 0) {
-    if (milis() - lighting_stop_time_ms > ligth_switch_close_time_ms) {
-      lighting_state = 1;
-      lighting_start_time_ms = milis();
-      lighting_state_changed = 1;
-    }
-  } else if (lighting_state == 1) {
-    if (milis() - lighting_start_time_ms > light_switch_open_time_ms) {
-      lighting_state = 0;
-      lighting_stop_time_ms = milis();
-      lighting_state_changed = 1;
-    }
-  }
-
-  // adjust servos to the current state if changed
-  if (lighting_state_changed == 1) {
-    if (lighting_state == 0) {
-      light_switch_servo.write(light_switch_angle_close)
-    } else if (lighting_state == 1) {
-      light_switch_servo.write(light_switch_angle_open)
-    }
-
-    if (watering_state == 0) {
-      water_valve_servo.write(water_valve_angle_close)
-    } else if (watering_state == 1) {
-      water_valve_servo.write(water_valve_angle_open)
-    }
-  }
-
-  delay(100);
 }
 
-// // Function to handle lighting state changes
-// void handleLightingState() {
-//   unsigned long currentMillis = milis();
 
-//   // Check if lighting needs to be turned on (state 0 -> 1)
-//   if (lighting_state == 0 && currentMillis - lighting_stop_time_ms > ligth_switch_close_time_ms) {
-//     lighting_state = 1;
-//     lighting_start_time_ms = currentMillis;
-//     lighting_state_changed = 1;
-//   }
-//   // Check if lighting needs to be turned off (state 1 -> 0)
-//   else if (lighting_state == 1 && currentMillis - lighting_start_time_ms > light_switch_open_time_ms) {
-//     lighting_state = 0;
-//     lighting_stop_time_ms = currentMillis;
-//     lighting_state_changed = 1;
-//   }
-// }
+void handleLightingStateBySchedule() {
+  unsigned long currentMillis = millis();
+  if (lighting_state == 0 &&
+      currentMillis - lighting_stop_time_ms > light_switch_close_time_ms
+  ) {
+    lighting_state = 1;
+    lighting_start_time_ms = currentMillis;
+    lighting_state_changed = 1;
+  } else if (lighting_state == 1 &&
+      currentMillis - lighting_start_time_ms > light_switch_open_time_ms
+  ) {
+    lighting_state = 0;
+    lighting_stop_time_ms = currentMillis;
+    lighting_state_changed = 1;
+  }
+}
 
-// // Function to adjust servos based on state
-// void adjustServos() {
-//   if (lighting_state_changed == 1) {
-//     // Adjust lighting servo based on state
-//     light_switch_servo.write(lighting_state == 0 ? light_switch_angle_close : light_switch_angle_open);
 
-//     // Adjust watering valve servo based on watering state
-//     water_valve_servo.write(watering_state == 0 ? water_valve_angle_close : water_valve_angle_open);
+void handleWateringStateByValue(double air_humidity) {
+  unsigned long int currentMillis = millis();
+  if (watering_state == 0 &&
+      air_humidity < air_humidity_treshold &&
+      currentMillis - watering_stop_time_ms > water_valve_closed_time_ms
+  ) {
+    watering_state = 1;
+    watering_start_time_ms = currentMillis;
+    watering_state_changed = 1;
+  } if (watering_state == 1 &&
+      currentMillis - watering_start_time_ms > water_valve_open_time_ms
+  ) {
+    watering_state = 0;
+    watering_stop_time_ms = currentMillis;
+    watering_state_changed = 1;
+  }
+}
 
-//     // Reset the state change flag after adjustment
-//     lighting_state_changed = 0;
-//   }
-// }
 
-// void loop() {
-//   // Handle lighting state changes
-//   handleLightingState();
+void adjustServos() {
+  if (lighting_state_changed == 1) {
+    light_switch_servo.write(
+      lighting_state == 0 ? light_switch_angle_close : light_switch_angle_open
+    );
+    water_valve_servo.write(
+      watering_state == 0 ? water_valve_angle_close : water_valve_angle_open
+    );
+    lighting_state_changed = 0;
+  }
+}
 
-//   // Adjust servos if any state change occurred
-//   adjustServos();
-// }
+
+void loop() {
+  // read sensors
+  double air_humidity = analogRead(air_humidity_sensor_pin) * 1.9 / 3.3;
+  double soil_humidity = 104 - (analogRead(soil_humidity_sensor_pin) / 7);
+  int air_temperature = analogRead(air_temperature_sensor_pin);
+
+  sendSensorData(air_humidity, soil_humidity, air_temperature);
+  switch (mode) {
+    case 0:
+      receiveControlMessage();
+      break;
+    case 1:
+      handleLightingStateBySchedule();
+      handleWateringStateByValue(air_humidity);
+      adjustServos();
+      break;
+    default:
+      handleLightingStateBySchedule();
+      handleWateringStateByValue(air_humidity);
+      adjustServos();
+      break;
+  }
+  
+  delay(100);
+}
