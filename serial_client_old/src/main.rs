368676918f20eab5b3bb10d04b6da6dd043e92bc
Franciszek Adamski <fraadamski@task.gda.pl>
2025-02-21 18:10:03 +0100

    fix finction errors

--- serial_client_old/src/main.rs
+++ serial_client_old/src/main.rs
@@ -1,18 +1,19 @@
+use std::io;
 use serial2::SerialPort;
 
-fn serial_request(port: &SerialPort, message: &str) -> Result<Vec<f64>, std::io::Error> {
+fn serial_request(port: &SerialPort, message: &str) -> Result<Vec<f64>, io::Error> {
     let mut buffer = [0; 256];
     let serial_request_message = message.as_bytes();
     buffer[..serial_request_message.len()]
         .copy_from_slice(serial_request_message);
 
-    match port.write(&buffer[..serial_request_message.len()]) {
-        Ok(_) => println!("Request sent successfully"),
-        Err(e) => eprintln!("Error sending request: {e:?}"),
-    }
+    port.write_all(serial_request_message)?;
 
-    let read = port.read(&mut buffer).unwrap();
-    let message = std::str::from_utf8(&buffer[..read]).unwrap();
+    let read = port.read(&mut buffer)?;
+    let message = match std::str::from_utf8(&buffer[..read]) {
+        Ok(msg) => msg.trim(),
+        Err(_) => return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid UTF-8 data")),
+    };
 
     let values: Vec<f64> = message
         .trim()
@@ -20,62 +21,22 @@ fn serial_request(port: &SerialPort, message: &str) -> Result<Vec<f64>, std::io:
         .filter_map(|v| v.parse::<f64>().ok())
         .collect();
 
-    match values.len() == 4 {
-        true => {
-            let air_humidity_percent = values[0];
-            let soil_humidity_percent = values[1];
-            let air_temperature_c_degrees = values[2];
-            let time_h = values.get(3).map(|&time_ms| time_ms / (1000.0 * 60.0 * 60.0)).unwrap_or(0.0);
-            let results = vec![
-                air_humidity_percent,
-                soil_humidity_percent,
-                air_temperature_c_degrees,
-                time_h
-            ]
-        },
-        false => {
-            eprintln!("Invalid data received: {message:?}");
-        },
+    if values.len() == 4 {
+        let air_humidity_percent = values[0];
+        let soil_humidity_percent = values[1];
+        let air_temperature_c_degrees = values[2];
+        let time_h = values.get(3).map(|&time_ms| time_ms / (1000.0 * 60.0 * 60.0)).expect("Could not convert ms to h");
+        Ok(vec![
+            air_humidity_percent,
+            soil_humidity_percent,
+            air_temperature_c_degrees,
+            time_h
+        ])
+    } else {
+        Err(io::Error::new(io::ErrorKind::InvalidData, format!("Invalid data received: {message}")))
     }
 }
 
-// fn main() {
-//     let port = SerialPort::open("/dev/ttyACM0", 9600)
-//         .expect("Could not open serial connection");
-//     let mut buffer = [0; 256];
-
-//     let serial_request_message = b"GET_SENSOR_DATA";
-//     buffer[..serial_request_message.len()]
-//         .copy_from_slice(serial_request_message);
-
-//     match port.write(&buffer[..serial_request_message.len()]) {
-//         Ok(_) => println!("Request sent successfully"),
-//         Err(e) => eprintln!("Error sending request: {e:?}"),
-//     }
-
-//     let read = port.read(&mut buffer).unwrap();
-//     let message = std::str::from_utf8(&buffer[..read]).unwrap();
-
-//     let values: Vec<f64> = message
-//         .trim()
-//         .split("-")
-//         .filter_map(|v| v.parse::<f64>().ok())
-//         .collect();
-
-//     if values.len() == 4 {
-//         let air_humidity_percent = values[0];
-//         let soil_humidity_percent = values[1];
-//         let air_temperature_c_degrees = values[2];
-//         let time_h = values.get(3).map(|&time_ms| time_ms / (1000.0 * 60.0 * 60.0)).unwrap_or(0.0);
-
-//         println!("Air humidity: {air_humidity_percent}%");
-//         println!("Soil humidity: {soil_humidity_percent}%");
-//         println!("Air temperature: {air_temperature_c_degrees} C");
-//         println!("Up time: {time_h} h");
-//     } else {
-//         eprintln!("Invalid data received: {message:?}");
-//     }
-// }
 
 fn main() {
     let port = SerialPort::open("/dev/ttyACM0", 9600)
